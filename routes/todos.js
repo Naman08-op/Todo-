0f73f6d78fca5bcbdab06ba310223152a2c5e625
namann08-op <naman.chhajed008@gmail.com>
2021-07-03 19:00:37 +0530

    GET request returns todos of a particular UserID

--- routes/todos.js
+++ routes/todos.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const router= express.Router()
-const Todo = require('../models/todo')
+const Todo = require('../models/todo_schema')
+
+router.route('/todos/:id')
+
+
 
 router.get('/',async(req,res)=>{
+     try{
+         const todos = await Todo.find()
+         res.json(todos)
+
+     }catch(err){
+         res.send('Error '+err)
+     }
+ })
+
+ router.get('/todos/:username',async(req,res)=>{
     try{
-        const todos = await Todo.find()
+        const todos = await Todo.findById(req.params.username)
         res.json(todos)
 
     }catch(err){
@@ -25,6 +39,21 @@ router.post('/',async(req,res)=>{
         res.send("ERROR"+ err)
     }
 })
+
+ router.post('/todo/:username',async(req,res)=>{
+    const todo = new Todo({     
+    todos: req.params.todos,
+         todo: req.params.todo
+    })
+     try{
+        const a1= await todo.save()
+        res.json(a1)
+     }catch(err){
+        res.send("ERROR"+ err)
+     }
+    })
+
+
 
 
 module.exports = router
